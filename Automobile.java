b12f4d6c21e44e3394cd95c5547c3db2fd2a835d
Markos-A <78107632+Markos-A@users.noreply.github.com>
2021-04-27 12:54:33 -0400

    Add files via upload

---
+++ Automobile.java
@@ -0,0 +1,28 @@
+
+package Assignments;
+
+/**
+ *
+ * @author markose aycheh
+ */
+
+public class Automobile extends TransportVehicle{
+    private double wheelBaseInches;
+
+    /**
+     * @return the wheelBaseInches
+     */
+    public double getWheelBaseInches() {
+        return wheelBaseInches;
+    }
+
+    /**
+     * @param wheelBaseInches the wheelBaseInches to set
+     */
+    public void setWheelBaseInches(double wheelBaseInches) {
+        this.wheelBaseInches = wheelBaseInches;
+    }
+    
+    
+    
+}
